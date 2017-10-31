d5aad81e2287234ca5fe15b3c11b25c0a0ab561f
=quantbhn93 <=>
2017-10-31 14:59:44 +0700

    Add solution hehe

---
+++ DojoDemo/Controllers/DojoController.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DojoDemo.Controllers
+{
+    public class DojoController : Controller
+    {
+        public ActionResult Dialog()
+        {
+            return View();
+        }
+
+        public ActionResult Config()
+        {
+            return View();
+        }
+
+        public ActionResult Modern ()
+        {
+            return View();
+        }
+
+        public ActionResult EventPubSub()
+        {
+            return View();
+        }
+
+        public ActionResult Aspect()
+        {
+            return View();
+        }
+
+        public ActionResult Array()
+        {
+            return View();
+        }
+
+        public ActionResult Hash()
+        {
+            return View();
+        }
+
+        public ActionResult Router()
+        {
+            return View();
+        }
+
+        public ActionResult CustomWidget1()
+        {
+            return View();
+        }
+
+        public ActionResult SomeWidget()
+        {
+            return View();
+        }
+
+
+    }
+}
